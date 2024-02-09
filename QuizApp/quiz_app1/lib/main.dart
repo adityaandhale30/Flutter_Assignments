23c0ffe988045d504ddfd95b70d9cc7c9e374a6d
AdiByte30 <adityaandhale30@gmail.com>
2024-02-09 21:53:28 +0530

    QuizApp Start Ui

--- QuizApp/quiz_app1/lib/main.dart
+++ QuizApp/quiz_app1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:quiz_app1/ListviewBuilder.dart';
-import 'DispInt.dart';
+//import 'DispInt.dart';
 
 void main() => runApp(const MainApp());
 
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DispInt(),
+      home: QuizApp(),
     );
   }
 }
@@ -24,87 +24,201 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
-  //const _QuizAppState({super.key});
+// Variable Decleared Here
 
-  int _counter = 1;
+  int _counter = 0;
+  int selectedIndex = -1;
+  bool questionScrren = true;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My QuizApp"),
-        backgroundColor: const Color.fromARGB(255, 111, 227, 250),
-        centerTitle: true,
-      ),
-      /*body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+  // List of Questions And Options And Answers
+
+  List<Map> questionList = [
+    {
+      "Questions": " Who is Founder of Microsoft ?",
+      "Option": ["Larray Page ", "steve Jobs", "bill gates", "Jeff bezoz"],
+      "answer": 2,
+    },
+    {
+      "Questions": "Who is Founder of Apple ?",
+      "Option": ["Elon musk", "Bill Gates", "Mark Z", "Steve Jobs"],
+      "answer": 3,
+    },
+    {
+      "Questions": "Who Founder of tesla ? ",
+      "Option": ["Pranav Dumbre", "Steve Jobs", "Elon musk", "Harshad anbhule"],
+      "answer": 2,
+    },
+    {
+      "Questions": "Who is Founder of Meta  ?",
+      "Option": ["Mark Z", "Elon Musk", "Bill gates", "Larray page"],
+      "answer": 0,
+    },
+    {
+      "Questions": "What is Harshad Anbhule's Pointer ?",
+      "Option": ["6.5", "7.0", "EFT Back", "9.7"],
+      "answer": 2,
+    }
+  ];
+
+  // Method That Returns Scaffold for Question And Celebration
+
+  Scaffold screenScaffold() {
+    if (questionScrren == true) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "QuizApp",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+                color: Color.fromARGB(255, 239, 145, 3),
+                letterSpacing: 5),
+          ),
+          backgroundColor: const Color.fromARGB(255, 188, 225, 242),
+          centerTitle: true,
+        ),
+
+        // Body of Questions And options
+
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(
-              height: 20,
+              height: 25,
             ),
-            Text("Question  : $_counter / 10 "),
-            const SizedBox(
-              height: 20,
-            ),
-            Text("Question $_counter :  What is Flutter"),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Option 1"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Option 2"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Question  : ",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "${_counter + 1} / ${questionList.length}",
+                  style: const TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.w600),
+                ),
+              ],
             ),
             const SizedBox(
-              height: 20,
+              height: 50,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Option 3"),
+            SizedBox(
+              width: 350,
+              height: 60,
+              child: Text(
+                questionList[_counter]["Questions"],
+                style:
+                    const TextStyle(fontSize: 23, fontWeight: FontWeight.w300),
+              ),
             ),
             const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
               width: 300,
-              height: 20,
-              // child:Container(color: Colors.black,),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                },
+
+                // First Option of Question
+
+                child: Text(
+                  "A. ${questionList[_counter]["Option"][0]}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.normal),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Option 4"),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(                                            // Elevated Button for 1st Option
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 1;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+
+                    (selectedIndex == questionList[_counter]["answer"]) ?  
+                      backgroundColor: Colors.green 
+                         
+                   :backgroundColor: Colors.red),
+                child: Text(
+                  "B. ${questionList[_counter]["Option"][1]}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 2;
+                  });
+                },
+                child: Text(
+                  "C. ${questionList[_counter]["Option"][2]}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 3;
+                  });
+                },
+                //style: ButtonStyle(),
+                child: Text(
+                  "D. ${questionList[_counter]["Option"][3]}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-            if (_counter == 11) _counter = 1;
-          });
-        },
-        child: const Icon(Icons.double_arrow_sharp),
-      ),*/
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              if (_counter != questionList.length - 1) {
+                _counter++;
+              } else {
+                _counter = 0;
+              }
+            });
+          },
+          child: const Icon(Icons.arrow_forward),
+        ),
+      );
+    } else {
+      return Scaffold();
+    }
+  }
 
-      body: Column(
-
-          // height: 400,
-          // width: 300,
-          children: [
-            Row(),
-            SizedBox(
-              width: 300,
-              height: 40,
-              child: Container(
-                color: Colors.amberAccent,
-              ),
-            ),
-          ]),
-    );
+  @override
+  Widget build(BuildContext context) {
+    return screenScaffold();
   }
 }
