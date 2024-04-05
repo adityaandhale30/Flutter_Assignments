d61c7a868e27c37ccc7748b44403cd8ea21f2deb
AdiByte30 <adityaandhale30@gmail.com>
2024-04-06 00:44:36 +0530

    Plant App first push

---
+++ Flutter_Projects/plant/lib/starting.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Starting extends StatefulWidget {
+  const Starting({super.key});
+  @override
+  State createState() => _StartingState();
+}
+
+class _StartingState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 80,
+              ),
+              child: Image.asset(
+                "assets/plant1.png",
+                width: 360,
+                height: 464,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 90,
+              ),
+              child: Row(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Enjoy your",
+                    style: GoogleFonts.poppins(
+                        fontSize: 34, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 90,
+              ),
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "life with ",
+                    style: GoogleFonts.poppins(
+                        fontSize: 34, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    "Plants",
+                    style: GoogleFonts.poppins(
+                        fontSize: 34, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: 320,
+                height: 50,
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 0, 0, 0.15),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(62, 102, 24, 1),
+                      Color.fromRGBO(124, 180, 70, 1),
+                    ],
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    " Get Started  > ",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
